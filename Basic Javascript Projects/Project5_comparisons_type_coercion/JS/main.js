3d0c633d8c2f71d37aad620ae637d87d90882818
Joe Gedeon <122491611+JoeGedeon@users.noreply.github.com>
2023-05-08 00:10:19 -0400

    Basic JavaScript Projects\Project5
    Basic JavaScript Projects\Project5_comparisons_type_coercion\index.html

--- Basic Javascript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic Javascript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,4 +1,7 @@
 // JavaScript source code
+
+
+
 function my_Dictionary() {
     var Animal = {
         Species: "Feline",
@@ -74,4 +77,50 @@ function not_Function() {
 
 function not_Function() {
     document.getElementById("Not").innerHTML = !(5 > 10);
+}
+
+
+function Ride_Funtion() {
+    var Height, Can_ride;
+    Height = document.getElementById("Height").value;
+    Can_ride = (Height < 52) ? "You are too short" : "You are tall enough";
+    document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
+}
+
+class Customer {
+    string FullName;
+    Boolean Active;
+    Customer(string name){
+    FullName = name;
+    Active = true;
+}
+}
+Customer cust = new Customer("Brenda Smith");
+
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+
+}
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustang");
+function myFunction() {
+    document.getElementById("Keywords_and_Constructors").innerHTML =
+        "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model +
+        " manufactured in " + Erik.Vehicle_Year;
+}
+
+var X = 10;
+var Y = "Charlie";
+function count_Function() {
+    document.getElementById("Counting").innerHTML = Count();
+    function Count() {
+        var Starting_point = 9;
+        function Plus_one() { Starting_point += 1; }
+        Plus_one();
+        return Starting_point;
+    }
 }
