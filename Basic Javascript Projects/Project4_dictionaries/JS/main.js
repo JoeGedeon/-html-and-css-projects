04797a0bb66f9a8b8967db3e4e48f7d0bf1cd9e4
Joe Gedeon <122491611+JoeGedeon@users.noreply.github.com>
2023-05-09 20:47:11 -0400

    Update main.js
    Added a delete file.

--- Basic Javascript Projects/Project4_dictionaries/JS/main.js
+++ Basic Javascript Projects/Project4_dictionaries/JS/main.js
@@ -14,3 +14,19 @@ function my_Dictionary() {
     };
     document.getElementById("Dictionary").innerHTML = Animal.Sound;
 }
+
+var A = "";
+document.write(A);
+
+
+function my_Dictionary() {
+    var Animal = {
+        Species: "Dog",
+        Color: "Black",
+        Breed: "Labrador",
+        Age: 5,
+        Sound: "Bark!"
+    };
+    delete Animal.Sound;
+    document.getElementById("Dictionary").innerHTML = Animal.Sound;
+}
