4dcf190d570149f723cc1c37dea0e843ab367963
Joe Gedeon <122491611+JoeGedeon@users.noreply.github.com>
2023-05-10 19:33:13 -0400

    update
    Basic Javascript Projects\Project6_ternary_operators_constructors\JS\main.js

--- Basic Javascript Projects/Project6_ternary_operators_constructors/JS/main.js
+++ Basic Javascript Projects/Project6_ternary_operators_constructors/JS/main.js
@@ -12,6 +12,18 @@ function count_Function() {
 }
 
 var X = 10;
+var Y = "Charlie";
+function count_Function() {
+    document.getElementById("Counting").innerHTML = Count();
+    function Count() {
+        var Starting_point = 9;
+        function Plus_one() { Starting_point += 1; }
+        Plus_one();
+        return Starting_point;
+    }
+}
+
+var X = 10;
 function Add_numbers_1() {
     document.write(20 + X + "<br>");
 }
