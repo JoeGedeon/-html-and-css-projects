7e8efbb812c19bef94d38c28ce90778eca18deaf
Joe Gedeon <122491611+JoeGedeon@users.noreply.github.com>
2023-05-10 20:32:43 -0400

    Project7
    Basic JavaScript Projects\Project7_scope_time_function\index.html

---
+++ Basic Javascript Projects/Project7_scope_time_function/JS/main.js
@@ -0,0 +1,13 @@
+function Time_function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning time!";
+    }
+    else if (Time >= 12 == Time < 18) {
+    }
+    else {
+        Reply = "It is evening time!";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
+}
