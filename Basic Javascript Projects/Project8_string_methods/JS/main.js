1a39ea24e33dd41bf5b07136689307e4bf2079dd
Joe Gedeon <122491611+JoeGedeon@users.noreply.github.com>
2023-05-11 20:54:23 -0400

    update

--- Basic Javascript Projects/Project8_string_methods/JS/main.js
+++ Basic Javascript Projects/Project8_string_methods/JS/main.js
@@ -1,8 +1,24 @@
-function full_Sentence() {
+function full_Sentence() { //full_Sentence section
     var part_1 = "I have ";
     var part_2 = "made this ";
     var part_3 = "into a complete ";
     var part_4 = "sentence. ";
     var whole_sentence = part_1.concat(part_2, part_3, part_4);
     document.getElementById("Concatenate").innerHTML = whole_sentence;
+}
+
+function slice_Method() {  //slice_Method section
+    var Sentence = "All work and no play makes Johnny a dull boy.";
+    var Section = Sentence.slice(27,33);
+    document.getElementById("Slice").innerHTML = Section;
+}
+
+function string_Method() { //string_Method section
+    var X =182;
+    document.getElementById("Numbers_to_sting").innerHTML =X.toString();
+}
+
+function precision_Method() { //precision_Method section
+    var X = 12938.3012987376112;
+    document.getElementById("Precision").innerHTML = X.toPrecision(10);
 }
