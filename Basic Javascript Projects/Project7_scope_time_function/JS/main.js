a84af1ae1576187950246dbca1ec9a3d99fb297c
Joe Gedeon <122491611+JoeGedeon@users.noreply.github.com>
2023-05-11 20:19:19 -0400

    update

--- Basic Javascript Projects/Project7_scope_time_function/JS/main.js
+++ Basic Javascript Projects/Project7_scope_time_function/JS/main.js
@@ -1,13 +1,26 @@
-function Time_function() {
+var X ="Hello";
+
+function Time_function() { //Time functions
     var Time = new Date().getHours();
     var Reply;
     if (Time < 12 == Time > 0) {
         Reply = "It is morning time!";
     }
     else if (Time >= 12 == Time < 18) {
+        Reply= "It is afternoon!";
     }
     else {
         Reply = "It is evening time!";
     }
     document.getElementById("Time_of_day").innerHTML = Reply;
+}
+
+function if_function() { // This is the if function
+    if (5 > 4 ) {
+        document.getElementById("greater").innerHTML = "5 is greater than 4."
+    }
+}
+
+function error_function() { // This is the error function
+     //console.log(Reply); comment this to get rid of the error.
 }
